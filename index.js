c702ee52783442ab2249990924b50e055be92583
Elliot Persson <elliot.thomas.persson@gmail.com>
2024-02-06 13:31:47 +0100

    made astronomy picture of the day appear in apod.html, changes based on what date it is

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const offScreenMenu = document.querySelector(".offScreenMenu")
 hamburgerMenu.addEventListener("click", () => {
   hamburgerMenu.classList.toggle("active");
   offScreenMenu.classList.toggle("active");
-  
+
 })
 
 
@@ -18,19 +18,23 @@ let searchButton = document.querySelector("#searchButton");
 searchButton.addEventListener("click", () => {
   console.log("You clicked the button");
   sendApiRequest();
+
 });
 
 
 //fetch data from API.
 async function sendApiRequest() {
-    let APIKey = "wj75V8DCvyHdQaZHLukFXkQWXDCS4iZ8GxHJgYPW"
-
-  let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${APIKey}`);
+  
+  const input = document.querySelector('input').value
+  console.log(input)
+  
+  let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=wj75V8DCvyHdQaZHLukFXkQWXDCS4iZ8GxHJgYPW&date=${input}`);
   console.log(response);
   let data = await response.json()
   console.log(data)
+  useApiData(data)
 }
 
 function useApiData(data){
-
+document.querySelector(".image").innerHTML += `<img src="${data.url}">`
 }
