0ed00565d3ee0cf05770b3e35b8b54b2fab70b55
Elliot Persson <elliot.thomas.persson@gmail.com>
2024-02-08 09:00:26 +0100

    created a new folder to store javascript files

---
+++ js/mars.js
@@ -0,0 +1,12 @@
+//Hamburger menu:
+const hamburgerMenu = document.querySelector(".hamburgerMenu");
+
+const offScreenMenu = document.querySelector(".offScreenMenu");
+
+hamburgerMenu.addEventListener("click", () => {
+  hamburgerMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+
+//mars rover photos:
+
